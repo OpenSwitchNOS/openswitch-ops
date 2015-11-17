e744882d3de210c2fe24773983998a28dcbb674d
Jose Hernandez <jphernandez@hpe.com>
2015-11-17 08:21:19 -0800

    Changed 2 test cases for LAG feature.
    Change-Id: Ie7dba2503b15bfc324353bb3aefb7d13ffa85781
    Signed-off-by: Jose Hernandez <jphernandez@hpe.com>

---
+++ tests/lag/static/test_lag_static_ft_convert_to_dynamic.py
@@ -0,0 +1,370 @@
+# (C) Copyright 2015 Hewlett Packard Enterprise Development LP
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+#
+###############################################################################
+# Name:       test_lag_static_ft_convert_to_dynamic.py
+#
+# Description: Tests that a previously configured dynamic Link Aggregation can
+#              be converted to a static one
+#
+# Author:      Jose Hernandez
+#
+# Topology:  |Host| ----- |Switch| ---------------------- |Switch| ----- |Host|
+#                                   (Dynamic LAG - 2 links)
+#
+# Success Criteria:  PASS -> LAGs is converted from dynamic to static
+#
+#                    FAILED -> LAG cannot be converted from dynamic to static
+#
+###############################################################################
+
+import pytest
+from opstestfw import *
+from opstestfw.switch.CLI import *
+from lib_test import lagVerifyTrafficFlow
+from lib_test import lagVerifyLACPStatusAndFlags
+from lib_test import lagVerifyConfig
+from lib_test import switchEnableInterface
+from lib_test import lagCreate
+from lib_test import vlanAddInterface
+from lib_test import vlanConfigure
+from lib_test import workstationConfigure
+from lib_test import switchReboot
+from lib_test import lagVerifyNumber
+from time import sleep
+from lib_test import interfaceEnableRouting
+from lib_test import interfaceGetStatus
+
+topoDict = {"topoExecution": 3000,
+            "topoTarget": "dut01 dut02",
+            "topoDevices": "dut01 dut02 wrkston01 wrkston02",
+            "topoLinks": "lnk01:dut01:wrkston01,\
+                          lnk02:dut01:dut02,\
+                          lnk03:dut01:dut02,\
+                          lnk04:dut02:wrkston02",
+            "topoFilters": "dut01:system-category:switch,\
+                            dut02:system-category:switch,\
+                            wrkston01:system-category:workstation,\
+                            wrkston02:system-category:workstation,\
+                            wrkston01:docker-image: openswitch/ubuntutest,\
+                            wrkston02:docker-image: openswitch/ubuntutest"}
+
+
+def lagChangeMode(deviceObj, lagId, mode):
+    #Change LAG mode and verify configuration is consistent
+    retStructOriginal = lacpAggregatesShow(
+        deviceObj=deviceObj, lagId=str(lagId))
+    if retStructOriginal.returnCode() != 0:
+        return False
+    retStruct = lagMode(lagId=str(lagId), deviceObj=deviceObj, lacpMode=mode)
+    if retStruct.returnCode() != 0:
+        return False
+    if not lagVerifyConfig(deviceObj, str(lagId),
+                           interfaces=retStructOriginal.\
+                           valueGet(key=str(lagId))['interfaces'],
+                           lacpMode=mode,
+                           fallbackFlag=retStructOriginal.\
+                           valueGet(key=str(lagId))['fallbackFlag'],
+                           hashType=retStructOriginal.\
+                           valueGet(key=str(lagId))['hashType'],
+                           lacpFastFlag=True):
+        return False
+    return True
+
+
+def clean_up_devices(dut01Obj, dut02Obj, wrkston01Obj, wrkston02Obj):
+    # Clean up devices
+    LogOutput('info', "\n############################################")
+    LogOutput('info', "Device Cleanup - rolling back config")
+    LogOutput('info', "############################################")
+    finalResult = []
+    dut01 = {'obj': dut01Obj, 'links': ['lnk01', 'lnk02', 'lnk03'],
+             'wrkston_links': ['lnk01']}
+    dut02 = {'obj': dut02Obj, 'links': ['lnk02', 'lnk03', 'lnk04'],
+             'wrkston_links': ['lnk04']}
+
+    LogOutput('info', "Unconfigure workstations")
+    LogOutput('info', "Unconfiguring workstation 1")
+    finalResult.append(workstationConfigure(
+        wrkston01Obj,
+        wrkston01Obj.linkPortMapping['lnk01'], "140.1.1.10",
+        "255.255.255.0", "140.1.1.255", False))
+    LogOutput('info', "Unconfiguring workstation 2")
+    finalResult.append(workstationConfigure(
+        wrkston02Obj,
+        wrkston02Obj.linkPortMapping['lnk04'], "140.1.1.11",
+        "255.255.255.0", "140.1.1.255", False))
+
+    LogOutput('info', "Delete LAGs on DUTs")
+    finalResult.append(lagCreate(dut01Obj, '1', False, [], 'off'))
+    finalResult.append(lagCreate(dut02Obj, '1', False, [], 'off'))
+
+    LogOutput('info', "Enable routing on DUTs workstations links")
+    for dut in [dut01, dut02]:
+        LogOutput('info', "Configuring switch %s" % dut['obj'].device)
+        for link in dut['wrkston_links']:
+            finalResult.append(interfaceEnableRouting(dut['obj'],
+                                                      dut['obj'].\
+                                                      linkPortMapping[link],
+                                                      True))
+
+    LogOutput('info', "Disable interfaces on DUTs")
+    for dut in [dut01, dut02]:
+        LogOutput('info', "Configuring switch %s" % dut['obj'].device)
+        for link in dut['links']:
+            finalResult.append(switchEnableInterface(dut['obj'],
+                                                     dut['obj'].\
+                                                     linkPortMapping[link],
+                                                     False))
+
+    LogOutput('info', "Remove VLAN from DUTs")
+    finalResult.append(vlanConfigure(dut01Obj, 900, False))
+    finalResult.append(vlanConfigure(dut02Obj, 900, False))
+
+    for i in finalResult:
+        if not i:
+            LogOutput('error', "Errors were detected while cleaning " +
+                    "devices")
+            return
+    LogOutput('info', "Cleaned up devices")
+
+
+class Test_ft_convert_to_dynamic:
+
+    def setup_class(cls):
+        # Create Topology object and connect to devices
+        Test_ft_convert_to_dynamic.testObj =\
+            testEnviron(topoDict=topoDict)
+        Test_ft_convert_to_dynamic.topoObj =\
+            Test_ft_convert_to_dynamic.testObj.topoObjGet()
+
+    def teardown_class(cls):
+        # clean devices
+        clean_up_devices(
+            cls.topoObj.deviceObjGet(device="dut01"),
+            cls.topoObj.deviceObjGet(device="dut02"),
+            cls.topoObj.deviceObjGet(device="wrkston01"),
+            cls.topoObj.deviceObjGet(device="wrkston02"))
+        # Terminate all nodes
+        Test_ft_convert_to_dynamic.topoObj.terminate_nodes()
+
+    def test_rebootSwitches(self):
+        LogOutput('info', "\n############################################")
+        LogOutput('info', "Reboot the switches")
+        LogOutput('info', "############################################")
+        dut01Obj = self.topoObj.deviceObjGet(device="dut01")
+        devRebootRetStruct = switchReboot(dut01Obj)
+        if not devRebootRetStruct:
+            LogOutput('error', "Failed to reboot and clean Switch 1")
+            assert(devRebootRetStruct)
+        else:
+            LogOutput('info', "Passed Switch 1 Reboot piece")
+        dut02Obj = self.topoObj.deviceObjGet(device="dut02")
+        devRebootRetStruct = switchReboot(dut02Obj)
+        if not devRebootRetStruct:
+            LogOutput('error', "Failed to reboot and clean Switch 2")
+            assert(devRebootRetStruct)
+        else:
+            LogOutput('info', "Passed Switch 2 Reboot piece")
+
+    def test_enableDUTsInterfaces(self):
+        LogOutput('info', "\n############################################")
+        LogOutput('info', "Enable switches interfaces")
+        LogOutput('info', "############################################")
+        dut01Obj = self.topoObj.deviceObjGet(device="dut01")
+        dut02Obj = self.topoObj.deviceObjGet(device="dut02")
+        dut01 = {'obj': dut01Obj, 'links': ['lnk01', 'lnk02', 'lnk03']}
+        dut02 = {'obj': dut02Obj, 'links': ['lnk02', 'lnk03', 'lnk04']}
+        for dut in [dut01, dut02]:
+            LogOutput('info', "Configuring switch %s" % dut['obj'].device)
+            for link in dut['links']:
+                assert(switchEnableInterface(dut['obj'],
+                                             dut['obj'].linkPortMapping[link],
+                                             True))
+
+    def test_createLAGs(self):
+        LogOutput('info', "\n############################################")
+        LogOutput('info', "Create LAGs")
+        LogOutput('info', "############################################")
+        dut01Obj = self.topoObj.deviceObjGet(device="dut01")
+        dut02Obj = self.topoObj.deviceObjGet(device="dut02")
+        assert(lagCreate(dut01Obj, '1', True, [
+            dut01Obj.linkPortMapping['lnk02'],
+            dut01Obj.linkPortMapping['lnk03']], 'off'))
+        assert(lagCreate(dut02Obj, '1', True, [
+            dut02Obj.linkPortMapping['lnk02'],
+            dut02Obj.linkPortMapping['lnk03']], 'off'))
+        assert(lagVerifyNumber(dut01Obj, 1))
+        assert(lagVerifyNumber(dut02Obj, 1))
+
+    def test_configureVLANs(self):
+        LogOutput('info', "\n############################################")
+        LogOutput('info', "Configure VLANs on switches")
+        LogOutput('info', "############################################")
+        dut01Obj = self.topoObj.deviceObjGet(device="dut01")
+        dut02Obj = self.topoObj.deviceObjGet(device="dut02")
+        dut01 = {'obj': dut01Obj, 'links': ['lag 1',
+                                            dut01Obj.\
+                                            linkPortMapping['lnk01']]}
+        dut02 = {'obj': dut02Obj, 'links': ['lag 1',
+                                            dut02Obj.\
+                                            linkPortMapping['lnk04']]}
+        for dut in [dut01, dut02]:
+            LogOutput('info', 'Configure VLAN on %s' % dut['obj'].device)
+            assert(vlanConfigure(dut['obj'], 900, True))
+            for link in dut['links']:
+                assert(vlanAddInterface(dut['obj'], 900, True, link))
+
+    def test_verifyInterfaces(self):
+        LogOutput('info', "\n############################################")
+        LogOutput('info', "Verify switches interfaces are up")
+        LogOutput('info', "############################################")
+        dut01Obj = self.topoObj.deviceObjGet(device="dut01")
+        dut02Obj = self.topoObj.deviceObjGet(device="dut02")
+        dut01 = {'obj': dut01Obj, 'links': ['lnk01', 'lnk02', 'lnk03']}
+        dut02 = {'obj': dut02Obj, 'links': ['lnk02', 'lnk03', 'lnk04']}
+        for dut in [dut01, dut02]:
+            LogOutput('info', 'Device %s' % dut['obj'].device)
+            for link in dut['links']:
+                assert(interfaceGetStatus(dut['obj'],
+                                          dut['obj'].linkPortMapping[link]))
+
+    def test_configureWorkstations(self):
+        LogOutput('info', "\n############################################")
+        LogOutput('info', "Configure workstations")
+        LogOutput('info', "############################################")
+        wrkston01Obj = self.topoObj.deviceObjGet(device="wrkston01")
+        wrkston02Obj = self.topoObj.deviceObjGet(device="wrkston02")
+        LogOutput('info', "Configuring workstation 1")
+        assert(workstationConfigure(
+            wrkston01Obj,
+            wrkston01Obj.linkPortMapping[
+                'lnk01'], "140.1.1.10", "255.255.255.0", "140.1.1.255", True))
+        LogOutput('info', "Configuring workstation 2")
+        assert(workstationConfigure(
+            wrkston02Obj,
+            wrkston02Obj.linkPortMapping[
+                'lnk04'], "140.1.1.11", "255.255.255.0", "140.1.1.255", True))
+
+    def test_LAGFormation1(self):
+        LogOutput('info', "\n############################################")
+        LogOutput('info', "Test LAGs are correctly formed and pass traffic")
+        LogOutput('info', "############################################")
+        dut01Obj = self.topoObj.deviceObjGet(device="dut01")
+        dut02Obj = self.topoObj.deviceObjGet(device="dut02")
+        wrkston01Obj = self.topoObj.deviceObjGet(device="wrkston01")
+        wrkston02Obj = self.topoObj.deviceObjGet(device="wrkston02")
+        assert(lagVerifyLACPStatusAndFlags(dut01Obj, dut02Obj, False,
+                                           ['lnk02', 'lnk03'], 'lag1',
+                                           'lag1'))
+        assert(lagVerifyTrafficFlow(dut01Obj, dut02Obj, wrkston01Obj,
+                                    wrkston02Obj, '140.1.1.10',
+                                    '140.1.1.11', ['lnk02', 'lnk03'], 'lnk01',
+                                    'lnk04', '140.1.1'))
+
+    def test_lagChangeMode(self):
+        LogOutput('info', "\n############################################")
+        LogOutput('info', "Change LAGs from static to dynamic")
+        LogOutput('info', "############################################")
+        dut01Obj = self.topoObj.deviceObjGet(device="dut01")
+        dut02Obj = self.topoObj.deviceObjGet(device="dut02")
+        LogOutput('info', "Change LAG mode on dut01")
+        assert(lagChangeMode(dut01Obj, '1', 'active'))
+        LogOutput('info', "Change LAG mode on dut02")
+        assert(lagChangeMode(dut02Obj, '1', 'passive'))
+
+    def test_LAGFormation2(self):
+        LogOutput('info', "\n############################################")
+        LogOutput('info', "Test LAGs are correctly formed and pass traffic")
+        LogOutput('info', "############################################")
+        dut01Obj = self.topoObj.deviceObjGet(device="dut01")
+        dut02Obj = self.topoObj.deviceObjGet(device="dut02")
+        wrkston01Obj = self.topoObj.deviceObjGet(device="wrkston01")
+        wrkston02Obj = self.topoObj.deviceObjGet(device="wrkston02")
+        assert(lagVerifyLACPStatusAndFlags(dut01Obj, dut02Obj, True,
+                                           ['lnk02', 'lnk03'], 'lag1', 'lag1',
+                                           dut02Flags={'Activ': '0',
+                                                       'TmOut': '1',
+                                                       'Aggr': '1',
+                                                       'Sync': '1',
+                                                       'Col': '1',
+                                                       'Dist': '1',
+                                                       'Def': '0',
+                                                       'Exp': '0'}))
+        assert(lagVerifyTrafficFlow(dut01Obj, dut02Obj, wrkston01Obj,
+                                    wrkston02Obj, '140.1.1.10',
+                                    '140.1.1.11', ['lnk02', 'lnk03'], 'lnk01',
+                                    'lnk04', '140.1.1'))
+
+    def test_disableAndEnableInterfacesOfLAGs(self):
+        LogOutput('info', "\n############################################")
+        LogOutput(
+            'info', "Disable and re-enable interfaces associated to LAGs")
+        LogOutput('info', "############################################")
+        dut01Obj = self.topoObj.deviceObjGet(device="dut01")
+        dut02Obj = self.topoObj.deviceObjGet(device="dut02")
+        dut01 = {'obj': dut01Obj, 'links': ['lnk01', 'lnk02', 'lnk03']}
+        dut02 = {'obj': dut02Obj, 'links': ['lnk02', 'lnk03', 'lnk04']}
+        LogOutput('info', "Disable interfaces on DUTs")
+        for dut in [dut01, dut02]:
+            LogOutput('info', "Configuring switch %s" % dut['obj'].device)
+            for link in dut['links']:
+                assert(switchEnableInterface(dut['obj'],
+                                             dut['obj'].linkPortMapping[link],
+                                             False))
+        LogOutput('info', "Re-enable interfaces on DUTs")
+        for dut in [dut01, dut02]:
+            LogOutput('info', "Configuring switch %s" % dut['obj'].device)
+            for link in dut['links']:
+                assert(switchEnableInterface(dut['obj'],
+                                             dut['obj'].linkPortMapping[link],
+                                             True))
+        
+    def test_verifyInterfaces(self):
+        LogOutput('info', "\n############################################")
+        LogOutput('info', "Verify switches interfaces are up")
+        LogOutput('info', "############################################")
+        dut01Obj = self.topoObj.deviceObjGet(device="dut01")
+        dut02Obj = self.topoObj.deviceObjGet(device="dut02")
+        dut01 = {'obj': dut01Obj, 'links': ['lnk01', 'lnk02', 'lnk03']}
+        dut02 = {'obj': dut02Obj, 'links': ['lnk02', 'lnk03', 'lnk04']}
+        for dut in [dut01, dut02]:
+            LogOutput('info', 'Device %s' % dut['obj'].device)
+            for link in dut['links']:
+                assert(interfaceGetStatus(dut['obj'],
+                                          dut['obj'].linkPortMapping[link]))
+
+    def test_LAGFormation3(self):
+        LogOutput('info', "\n############################################")
+        LogOutput('info', "Test LAGs are correctly formed and pass traffic")
+        LogOutput('info', "############################################")
+        dut01Obj = self.topoObj.deviceObjGet(device="dut01")
+        dut02Obj = self.topoObj.deviceObjGet(device="dut02")
+        wrkston01Obj = self.topoObj.deviceObjGet(device="wrkston01")
+        wrkston02Obj = self.topoObj.deviceObjGet(device="wrkston02")
+        assert(lagVerifyLACPStatusAndFlags(dut01Obj, dut02Obj, True,
+                                           ['lnk02', 'lnk03'], 'lag1', 'lag1',
+                                           dut02Flags={'Activ': '0',
+                                                       'TmOut': '1',
+                                                       'Aggr': '1',
+                                                       'Sync': '1',
+                                                       'Col': '1',
+                                                       'Dist': '1',
+                                                       'Def': '0',
+                                                       'Exp': '0'}))
+        assert(lagVerifyTrafficFlow(dut01Obj, dut02Obj, wrkston01Obj,
+                                    wrkston02Obj, '140.1.1.10',
+                                    '140.1.1.11', ['lnk02', 'lnk03'], 'lnk01',
+                                    'lnk04', '140.1.1'))
