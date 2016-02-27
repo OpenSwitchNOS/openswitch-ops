3039f2a3a9d0f23223b2d79b1431509132dbf4c7
Kattia Chaves <kchavesr@hpe.com>
2016-02-27 09:07:11 -0800

    fix: dev: Fix names and locations for LAG FT
    This commit includes:
        Moving all LAG FT to one single directory
        Changing names to an standard version to comply with CIT
        Fixing minor errors with pep8
        Fixing compilations errors
    
    Change-Id: Ife34e79787d50a723b2bc451f04755449c28edf1
    Signed-off-by: Kattia Chaves <kchavesr@hpe.com>

--- tests/lag/test_ft_lacp_valid_invalid_names.py
+++ tests/lag/test_ft_lacp_valid_invalid_names.py
@@ -1,4 +1,4 @@
-# (C) Copyright 2015 Hewlett Packard Enterprise Development LP
+# (C) Copyright 2015-2016 Hewlett Packard Enterprise Development LP
 # All Rights Reserved.
 #
 #    Licensed under the Apache License, Version 2.0 (the "License"); you may
@@ -16,11 +16,11 @@
 
 
 ##########################################################################
-# Name:        lagStaticSupportedAndUnsupportedNames
+# Name:        lagDynamicSupportedAndUnsupportedNames
 #
-# Objective:   To verify a static LAG can be created using permitted names
+# Objective:   To verify a dynamic LAG can be created using permitted names
 #              and cannot be created if the name is too long or has
-#                unsupported characters
+#              unsupported characters
 #
 # Author:      Pablo Araya M.
 #
@@ -28,7 +28,6 @@
 #
 ##########################################################################
 
-import pytest
 from opstestfw import *
 from opstestfw.switch.CLI import *
 from opstestfw.host import *
@@ -39,74 +38,64 @@ topoDict = {"topoExecution": 120,
             "topoFilters": "dut01:system-category:switch"}
 
 
-def deviceCleanup(dut01Obj):
-    returnCode = 0
-
-    LogOutput('info', "############################################")
-    LogOutput('info', "CLEANUP: Reboot the switch")
-    LogOutput('info', "############################################")
-    rebootRetStruct = dut01Obj.Reboot()
-    rebootRetStruct = returnStruct(returnCode=0)
-
-    if rebootRetStruct.returnCode() != 0:
-        LogOutput('error', "Switch Reboot failed")
-        returnCode = 1
-    else:
-        LogOutput('info', "Passed Switch Reboot piece")
-
-    # Global cleanup return structure
-    cleanupRetStruct = returnStruct(returnCode=returnCode)
-    return cleanupRetStruct
-
 
 def configureLag(dut01Obj, lagName, negative):
+    retCode = 0
+
     LogOutput('info', "############################################")
-    LogOutput('info', "Configure LAG using name: " + str(lagName))
+    LogOutput('info', "Create LAG using name: " + str(lagName))
     LogOutput('info', "############################################")
 
-    retStruct = lagCreation(deviceObj=dut01Obj, lagId=lagName,
-                            configFlag=True)
+    retStruct = lagCreation(deviceObj=dut01Obj, lagId=lagName)
+    if retStruct.returnCode() != 0:
+        LogOutput('error', "Failed to create LAG " + str(lagName))
+        retCode = 1
+    else:
+        LogOutput('info', "LAG  " + str(lagName) + " successfully created")
+
+    LogOutput('info', "############################################")
+    LogOutput('info', "Configure LAG Mode using name: " + str(lagName))
+    LogOutput('info', "############################################")
+
+    retStruct = lagMode(deviceObj=dut01Obj, lagId=lagName, lacpMode="active")
 
     if negative:
-        if retStruct.returnCode() == 0:
+        if retStruct.returnCode() == 0 and retCode == 0:
             LogOutput('error', "LAG with invalid name " +
                       str(lagName) + " successfully configured")
             return False
         else:
             LogOutput('info', "LAG with invalid name " +
-                      str(lagName) + " was not configure, as expected")
+                      str(lagName) + " was not configured, as expected")
             return True
     else:
-        if retStruct.returnCode() != 0:
+        if retStruct.returnCode() != 0 or retCode != 0:
             LogOutput(
                 'error', "Failed to configure LAG with name " + str(lagName))
             return False
         else:
-            LogOutput(
-                'info', "Successfully configured LAG with name \
+            LogOutput('info', "Successfully configured LAG with name \
                 " + str(lagName))
             return True
 
-
-class Test_lagStaticSupportedAndUnsupportedNames:
+class Test_lagDynamicSupportedAndUnsupportedNames:
 
     # Global variables
     dut01Obj = None
 
     def setup_class(cls):
         # Create Topology object and connect to devices
-        Test_lagStaticSupportedAndUnsupportedNames.testObj = testEnviron(
+        Test_lagDynamicSupportedAndUnsupportedNames.testObj = testEnviron(
             topoDict=topoDict)
-        Test_lagStaticSupportedAndUnsupportedNames.topoObj = \
-            Test_lagStaticSupportedAndUnsupportedNames.testObj.topoObjGet()
+        Test_lagDynamicSupportedAndUnsupportedNames.topoObj = \
+            Test_lagDynamicSupportedAndUnsupportedNames.testObj.topoObjGet()
         # Global variables
         global dut01Obj
         dut01Obj = cls.topoObj.deviceObjGet(device="dut01")
 
     def teardown_class(cls):
         # Terminate all nodes
-        deviceCleanup(dut01Obj)
-        Test_lagStaticSupportedAndUnsupportedNames.topoObj.terminate_nodes()
+        Test_lagDynamicSupportedAndUnsupportedNames.topoObj.terminate_nodes()
 
     def test_configure_LAG_ValidName_1(self):
         lagName = 1
