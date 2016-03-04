d8b4ad5de87f05954e6dd458caee9bb59bae654d
Al Harrington <al.harrington@hpe.com>
2016-03-03 17:16:14 -0800

    new: dev: Test to validate data for web UI overview screen
    Change-Id: Ide3240a22f57942316ca06ccbd215fd3500f3d3e
    Signed-off-by: Al Harrington <al.harrington@hpe.com>

---
+++ tests/webui/test_webui_ft_overview.py
@@ -0,0 +1,123 @@
+#!/usr/bin/env python
+#
+# Copyright (C) 2016 Hewlett Packard Enterprise Development LP
+# All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+import pytest
+
+from opsvsi.docker import *
+from opsvsi.opsvsitest import *
+
+import json
+import httplib
+import urllib
+
+from webui.utils.utils import *
+
+NUM_OF_SWITCHES = 1
+NUM_HOSTS_PER_SWITCH = 0
+
+
+class myTopo(Topo):
+
+    def build(self, hsts=0, sws=1, **_opts):
+        self.hsts = hsts
+        self.sws = sws
+        switch = self.addSwitch("s1")
+
+
+class Test_OverviewData(OpsVsiTest):
+    def setupNet(self):
+        self.net = Mininet(topo=myTopo(hsts=NUM_HOSTS_PER_SWITCH,
+                           sws=NUM_OF_SWITCHES,
+                           hopts=self.getHostOpts(),
+                           sopts=self.getSwitchOpts()),
+                           switch=VsiOpenSwitch,
+                           host=None,
+                           link=None,
+                           controller=None,
+                           build=True)
+
+        self.SWITCH_IP = get_switch_ip(self.net.switches[0])
+        self.PATHBASE = "/rest/v1/system/subsystems/base"
+        self.PATHSYS = "/rest/v1/system"
+
+    def test_overview_data(self):
+        info("\n########## Test to Validate Base Info ##########\n")
+        # Get data
+        status_code, response_data = execute_request(self.PATHBASE, "GET",
+                                                     None,
+                                                     self.SWITCH_IP)
+        assert status_code == httplib.OK, "Failed to get base information"
+
+        # Verify Data
+        json_data = get_json(response_data)
+        other_info = json_data["status"]["other_info"]
+
+        assert 'Product Name' in other_info
+        assert 'part_number' in other_info
+        assert 'onie_version' in other_info
+        assert 'base_mac_address' in other_info
+        assert 'serial_number' in other_info
+        assert 'vendor' in other_info
+        assert 'max_interface_speed' in other_info
+        assert 'max_transmission_unit' in other_info
+        assert 'interface_count' in other_info
+
+        info("\n########## Base Data Validated ##########\n")
+
+        info("\n########## Test to Validate System Info ##########\n")
+        # Get data
+        status_code, response_data = execute_request(self.PATHSYS, "GET",
+                                                     None,
+                                                     self.SWITCH_IP)
+        assert status_code == httplib.OK, "Failed to get base information"
+
+        # Verify Data
+        json_data = get_json(response_data)
+        system_config = json_data["configuration"]
+        system_status = json_data["status"]
+
+        assert 'hostname' in system_config
+        assert 'switch_version' in system_status
+
+        info("\n########## System Data Validated ##########\n")
+
+
+class Test_OverviewPage:
+    def setup(self):
+        pass
+
+    def teardown(self):
+        pass
+
+    def setup_class(cls):
+        Test_OverviewPage.test_var = Test_OverviewData()
+        rest_sanity_check(cls.test_var.SWITCH_IP)
+
+    def teardown_class(cls):
+        Test_OverviewPage.test_var.net.stop()
+
+    def setup_method(self, method):
+        pass
+
+    def teardown_method(self, method):
+        pass
+
+    def __del__(self):
+        del self.test_var
+
+    def test_run(self):
+        self.test_var.test_overview_data()
