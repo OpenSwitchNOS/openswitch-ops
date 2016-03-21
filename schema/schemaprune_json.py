3d71e7c67ba58b49e75c5aa047ae58b2747ca4b0
Sagar Bhanagay <sagar.bhanagay@hpe.com>
2016-03-20 23:34:33 -0700

    chg: dev: Minor modifications to schema_pruning scripts
    Change-Id: I6d1a14b458a047107063880ab00bbbb55d64f781
    Signed-off-by: Sagar Bhanagay <sagar.bhanagay@hpe.com>

--- schema/schemaprune_json.py
+++ schema/schemaprune_json.py
@@ -1,22 +1,47 @@
 from types import *
 import json
 import sys
+import logging
 
-#Temporarily maintaining a static list
+# Globals
 enabled_features = []
+CAN_DELETE, CANNOT_DELETE, UNTAGGED = range(0, 3)
+
+def get_enabled_features():
+    with open(sys.argv[3]) as f:
+        for line in f:
+            for entry in line.split():
+                enabled_features.append(entry)
+
+    logging.info("================================================================================")
+    for feature in enabled_features:
+        logging.info("enabled feature: %s" % feature)
+    logging.info("================================================================================")
 
 if __name__ == '__main__':
     exit
 
 # sanitize the arguments
-if len(sys.argv) < 3:
-    print("Error: Script needs 2 argument (input-schema output-schema)")
+if len(sys.argv) < 4:
+    logging.critical("This script takes 3 arguments:")
+    logging.critical("1. input-schema json file")
+    logging.critical("2. output-schema json file")
+    logging.critical("3. file containing the enabled-features (space separated)")
+    logging.critical("The 4th argument is optional:")
+    logging.critical("4. Log-level for the script: DEBUG, INFO, WARNING, ERROR, CRITICAL")
     sys.exit(0)
+
+# Set the logging level
+log_level_list = ["DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"]
+if ((sys.argv[4:]) and (sys.argv[4] in log_level_list)):
+    logging.basicConfig(level=logging.sys.argv[4])
+else:
+    logging.basicConfig(level=logging.CRITICAL)
 
 # Create enabled_feature_list
 # Here we'll go thru the IMAGE_FEATURES variable & create the list.
 # For now we're testing with the following static list
-enabled_features = ["bgp", "ntp", "ntp_client"]
+get_enabled_features()
 
 # read the json ovs schema
 with open(sys.argv[1]) as x:
@@ -27,75 +52,93 @@ with open(sys.argv[1]) as x:
 # corresponding to features that have not been enabled
 tables = ovsschema['tables']
 for table in tables.keys():
-    print("table = %s" % table)
-    del_table = del_column = del_enum = False
+    logging.debug("================================================================================")
+    logging.debug("table: \"%s\"" % table)
+    del_table = del_column = del_enum = UNTAGGED
 
     table_data = tables[table]
     if 'feature_list' in table_data:
-        print("table = %s" % table)
         features = table_data['feature_list']
-        print("features = %s" % features)
+        logging.debug("features = %s" % features)
 
-        #if ((0 == cmp(features.sort(), enabled_features.sort())) or (set(features) < set(enabled_features))):
         if bool(set(features) & set(enabled_features)):
-            print("There is at least 1 feature in the feature_list that has been enabled")
+            logging.info("DELETE:N: (table: \"%s\")" % table)
+            del_table = CANNOT_DELETE
         else:
-            del_table = True
-            print("Table %s can be deleted" % table)
+            logging.info("DELETE:Y: (table: \"%s\")" % table)
+            del_table = CAN_DELETE
+
+        del table_data['feature_list']
+    else:
+        logging.debug("UNTAGGED: (table: \"%s\")" % table)
 
     if 'columns' in table_data:
         columns = table_data['columns']
         for column in columns.keys():
-            print("column = %s" % column)
+            logging.debug("column = %s" % column)
+            del_column = del_enum = UNTAGGED
             column_data = columns[column]
             if 'feature_list' in column_data:
-                print("table = %s; column = %s" % (table, column))
                 features = column_data['feature_list']
-                print("features = %s" % features)
+                logging.debug("features = %s" % features)
                 if bool(set(features) & set(enabled_features)):
-                    print("There is at least 1 feature in the feature_list that has been enabled")
-                    del_table = False
+                    logging.info("DELETE:N: (table: \"%s\"; column: \"%s\")" % (table, column))
+                    del_column = CANNOT_DELETE
+                    del_table = CANNOT_DELETE
                 else:
-                    del_column = True
-                    print("table %s column %s can be deleted" % (table, column))
-                    #columns.pop(column, None)
-                    #del_column = False
+                    logging.info("DELETE:Y: (table: \"%s\"; column: \"%s\")" % (table, column))
+                    del_column = CAN_DELETE
+
+                del column_data['feature_list']
+            else:
+                logging.debug("UNTAGGED: (table: \"%s\"; column: \"%s\")" % (table, column))
 
             if 'type' in column_data:
                 type_data = column_data['type']
-                print("type_data = %s" % type_data)
+                logging.debug("type_data = %s" % type_data)
                 if 'key' in type_data:
                     key_data = type_data['key']
-                    print("key_data = %s" % key_data)
+                    logging.debug("key_data = %s" % key_data)
                     if 'enum' in key_data:
                         enum_data = key_data['enum']
-                        print("enum_data = %s" % enum_data)
+                        logging.debug("enum_data = %s" % enum_data)
                         if 'set' in enum_data:
                             enum_index = -1
                             for elem in enum_data[1]:
                                 enum_index += 1
-                                print("enum = %s" % elem)
+                                logging.debug("enum = %s" % elem)
                                 if type(elem) is DictType:
-                                    if 'feature_list' in elem:
+                                    if 'feature_list' and 'val' in elem:
                                         features = elem['feature_list']
-                                        print("features = %s" % features)
+                                        enum_val = elem['val']
+                                        logging.debug("features = %s" % features)
+                                        logging.debug("enum_val = %s" % enum_val)
                                         if bool(set(features) & set(enabled_features)):
-                                            print("There is at least 1 feature in the feature_list that has been enabled")
-                                            del_column = False
-                                            del_table = False
+                                            logging.info("DELETE:N: (table: \"%s\"; column: \"%s\; enum: \"%s\")" % (table, column, enum_val))
+                                            del_enum = CANNOT_DELETE
+                                            del_column = CANNOT_DELETE
+                                            del_table = CANNOT_DELETE
                                         else:
-                                            del_enum = True
-                                            print("table %s column %s enum %s can be deleted" % (table, column, elem))
-                                            #enum_data.pop(elem, None)
-                                            enum_data[1].pop(enum_index)
-                                            del_enum = False
+                                            logging.info("DELETE:Y: (table: \"%s\"; column: \"%s\; enum: \"%s\")" % (table, column, enum_val))
+                                            del_enum = CAN_DELETE
 
-            if (del_column == True):
+                                        # Now delete the DictType entry from the enum
+                                        enum_data[1].pop(enum_index)
+                                        if (del_enum == CANNOT_DELETE):
+                                            # Add regular entry to the enum
+                                            enum_data[1].insert(enum_index, enum_val)
+                                    else:
+                                        logging.debug("UNTAGGED: (table: \"%s\"; column: \"%s\; enum: \"%s\")" % (table, column, enum_val))
+                                else:
+                                    logging.debug("UNTAGGED: (table: \"%s\"; column: \"%s\; enum: \"%s\")" % (table, column, elem))
+
+            if (del_column == CAN_DELETE):
                 columns.pop(column, None)
-                del_column = False
 
-    if (del_table == True):
+    if (del_table == CAN_DELETE):
         tables.pop(table, None)
+
+logging.info("================================================================================")
 
 # Generate the output schema file
 with open(sys.argv[2], 'w') as fo:
--- schema/schemaprune_xml.py
+++ schema/schemaprune_xml.py
@@ -1,55 +1,81 @@
 from types import *
 import sys
-from xml.dom.minidom import parse
 import xml.dom.minidom
+import logging
 
-#Temporarily maintaining a static list
+# Globals
 enabled_features = []
-local_features = []
-MAX_FEATURES = 50
+MAX_FEATURES = 100
+CAN_DELETE, CANNOT_DELETE, UNTAGGED = range(0, 3)
 
-def check_delete_object(objname):
+# Go through the file containing the list of (space separated) enabled-features
+# and populate the list
+def get_enabled_features():
+    with open(sys.argv[3]) as f:
+        for line in f:
+            for entry in line.split():
+                enabled_features.append(entry)
+
+    logging.info("================================================================================")
+    for feature in enabled_features:
+        logging.info("enabled feature: %s" % feature)
+    logging.info("================================================================================")
+
+
+def delete_object(obj):
+    parent = obj.parentNode
+    logging.debug("parent = %s" % parent)
+    parent.removeChild(obj)
+
+# Go through the feature_list defined in obj & return if the obj is deletable
+def check_delete_object(obj):
     local_features = ['']*MAX_FEATURES
-    feature_list = objname.getElementsByTagName("feature_list")
-    if (feature_list):
-        print("len: %s" % len(feature_list))
-        features = objname.getElementsByTagName("feature")
-        for i in range(0, (len(feature_list))):
-            feature = features[i]
-            print("%s" % feature.childNodes[0].nodeValue)
-            local_features[i] = str(feature.childNodes[0].nodeValue)
 
-        for i in range(0, len(feature_list)):
-            print("local_feature = %s" % local_features[i])
+    for child in obj.childNodes:
+        # Check for ELEMENT_NODE
+        if (child.nodeType == 1):
+            logging.debug(child.nodeName)
+            if (child.nodeName == "feature_list"):
+                feature_list = child.getElementsByTagName("feature")
+                logging.debug("len = %s" % len(feature_list))
+                for i in range(0, (len(feature_list))):
+                    feature = feature_list[i]
+                    local_features[i] = str(feature.childNodes[0].nodeValue)
+                for i in range(0, len(feature_list)):
+                    logging.debug("local_feature = %s" % local_features[i])
 
-        if bool(set(local_features) & set(enabled_features)):
-            print("There is at least 1 feature in the feature_list that has been enabled. Cannot delete object")
-            return False
-        else:
-            return True
-    else:
-        #This is base object. Cannot delete it
-        return False
+                delete_object(child)
+                if bool(set(local_features) & set(enabled_features)):
+                    logging.debug("There is at least 1 feature in the feature_list that has been enabled. Cannot delete object")
+                    return CANNOT_DELETE
+                else:
+                    return CAN_DELETE
 
-
-def delete_object(objname):
-    parent = objname.parentNode
-    print("parent = %s" % parent)
-    parent.removeChild(objname)
+    # If feature_list is not found, return "UNTAGGED"
+    return UNTAGGED
 
 
 if __name__ == '__main__':
     exit
 
 # sanitize the arguments
-if len(sys.argv) < 3:
-    print("Error: Script needs 2 argument (input-schema output-schema)")
+if len(sys.argv) < 4:
+    logging.critical("This script takes 3 arguments:")
+    logging.critical("1. input-schema xml file")
+    logging.critical("2. output-schema xml file")
+    logging.critical("3. file containing the enabled-features (space separated)")
+    logging.critical("The 4th argument is optional:")
+    logging.critical("4. Log-level for the script: DEBUG, INFO, WARNING, ERROR, CRITICAL")
     sys.exit(0)
 
-# Create enabled_feature_list
-# Here we'll go thru the IMAGE_FEATURES variable & create the list.
-# For now we're testing with the following static list
-enabled_features = ["bgp", "ntp", "ntp_client"]
+# Set the logging level
+log_level_list = ["DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"]
+if ((sys.argv[4:]) and (sys.argv[4] in log_level_list)):
+    logging.basicConfig(level=logging.sys.argv[4])
+else:
+    logging.basicConfig(level=logging.CRITICAL)
+
+get_enabled_features()
 
 # read the xml ovs schema
 DOMTree = xml.dom.minidom.parse(sys.argv[1])
@@ -58,53 +84,79 @@ DOMTree = xml.dom.minidom.parse(sys.argv[1])
 # corresponding to features that have not been enabled
 tables = DOMTree.getElementsByTagName("table")
 for table in tables:
-    del_table = del_group = del_column = False
+    del_table = del_group = del_column = UNTAGGED
 
-    print("table: %s" % table.getAttribute("name"))
+    logging.debug("================================================================================")
+    logging.debug("table: %s" % table.getAttribute("name"))
     del_table = check_delete_object(table)
+    if (del_table == CANNOT_DELETE):
+        logging.info("DELETE:N: (table: \"%s\")" % table.getAttribute("name"))
+    elif (del_table == UNTAGGED):
+        logging.debug("UNTAGGED: (table: \"%s\")" % table.getAttribute("name"))
 
-    groups = table.getElementsByTagName("group")
-    for group in groups:
-        print("group: %s" % group.getAttribute("title"))
-        del_group = check_delete_object(group)
+    for child in table.childNodes:
+        # Check for ELEMENT_NODE
+        if (child.nodeType == 1):
+            logging.debug(child.nodeName)
+            if (child.nodeName == "group"):
+                group = child
+                logging.debug("group: %s" % group.getAttribute("title"))
+                del_group = check_delete_object(group)
+                if (del_group == CANNOT_DELETE):
+                    logging.info("DELETE:N: (table: \"%s\"; group: \"%s\")" % \
+                                  (table.getAttribute("name"), group.getAttribute("title")))
+                    del_table = CANNOT_DELETE
+                elif (del_group == UNTAGGED):
+                    logging.debug("UNTAGGED: (table: \"%s\"; group: \"%s\")" % \
+                                  (table.getAttribute("name"), group.getAttribute("title")))
 
-        columns = group.getElementsByTagName("column")
-        for column in columns:
-            print("column: %s; key: %s" % (column.getAttribute("name"), column.getAttribute("key")))
+                columns = group.getElementsByTagName("column")
+                for column in columns:
+                    logging.debug("column: %s; key: %s" % (column.getAttribute("name"), column.getAttribute("key")))
 
-            del_column = check_delete_object(column)
-            if (del_column == True):
-                print("column %s; key: %s can be deleted" % (column.getAttribute("name"), column.getAttribute("key")))
-                delete_object(column)
-            else:
-                print("column %s; key: %s cannot be deleted" % (column.getAttribute("name"), column.getAttribute("key")))
-                #del_group = False
+                    del_column = check_delete_object(column)
+                    if (del_column == CAN_DELETE):
+                        logging.info("DELETE:Y: (table: \"%s\"; group: \"%s\"; column: \"%s\"; key: \"%s\")" % \
+                               (table.getAttribute("name"), group.getAttribute("title"), column.getAttribute("name"), column.getAttribute("key")))
+                        delete_object(column)
+                    elif (del_column == CANNOT_DELETE):
+                        logging.info("DELETE:N: (table: \"%s\"; group: \"%s\"; column: \"%s\"; key: \"%s\")" % \
+                               (table.getAttribute("name"), group.getAttribute("title"), column.getAttribute("name"), column.getAttribute("key")))
+                        del_group = CANNOT_DELETE
+                    elif (del_column == UNTAGGED):
+                        logging.debug("UNTAGGED: (table: \"%s\"; group: \"%s\"; column: \"%s\"; key: \"%s\")" % \
+                               (table.getAttribute("name"), group.getAttribute("title"), column.getAttribute("name"), column.getAttribute("key")))
 
-        if (del_group == True):
-            print("group %s can be deleted" % group.getAttribute("title"))
-            delete_object(group)
-        else:
-            print("group %s cannot be deleted" % group.getAttribute("title"))
-            #del_table = False
+                if (del_group == CAN_DELETE):
+                    logging.info("DELETE:Y: (table: \"%s\"; group: \"%s\")" % \
+                                  (table.getAttribute("name"), group.getAttribute("title")))
+                    delete_object(group)
+                elif (del_group == CANNOT_DELETE):
+                    logging.info("DELETE:N: (table: \"%s\"; group: \"%s\")" % \
+                                  (table.getAttribute("name"), group.getAttribute("title")))
+                    del_table = CANNOT_DELETE
 
-    #Now let's check the remaining columns
-    columns = table.getElementsByTagName("column")
-    for column in columns:
-        print("column: %s; key: %s" % (column.getAttribute("name"), column.getAttribute("key")))
-        del_column = check_delete_object(column)
-        if (del_column == True):
-            print("column %s; key: %s can be deleted" % (column.getAttribute("name"), column.getAttribute("key")))
-            delete_object(column)
-        else:
-            print("column %s; key: %s cannot be deleted" % (column.getAttribute("name"), column.getAttribute("key")))
-            #del_table = False
+            elif (child.nodeName == "column"):
+                column = child
+                logging.debug("column: %s; key: %s" % (column.getAttribute("name"), column.getAttribute("key")))
 
-    if (del_table == True):
-        print("table %s can be deleted" % table.getAttribute("name"))
+                del_column = check_delete_object(column)
+                if (del_column == CAN_DELETE):
+                    logging.info("DELETE:Y: (column: \"%s\"; key: \"%s\")" % (column.getAttribute("name"), column.getAttribute("key")))
+                    delete_object(column)
+                elif (del_column == CANNOT_DELETE):
+                    logging.info("DELETE:N: (column: \"%s\"; key: \"%s\")" % (column.getAttribute("name"), column.getAttribute("key")))
+                    del_table = CANNOT_DELETE
+                elif (del_column == UNTAGGED):
+                    logging.debug("UNTAGGED: (column: \"%s\"; key: \"%s\")" % (column.getAttribute("name"), column.getAttribute("key")))
+
+    if (del_table == CAN_DELETE):
+        logging.info("DELETE:Y: (table: \"%s\")" % table.getAttribute("name"))
         delete_object(table)
-    else:
-        print("table %s cannot be deleted" % table.getAttribute("name"))
+    elif (del_table == CANNOT_DELETE):
+        logging.info("DELETE:N: (table: \"%s\")" % table.getAttribute("name"))
 
+logging.info("================================================================================")
 
 # Generate the output schema file
 file_handle = open(sys.argv[2], "w")
