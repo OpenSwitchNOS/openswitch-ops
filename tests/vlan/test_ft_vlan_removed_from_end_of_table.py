aa6f1f933814aa341aab25fd040de241e5e0dc81
fonsecamau <fonsecamau@hpe.com>
2016-06-06 09:47:15 -0700

    chg: dev: Disabling tests. Tests were ported to modular framework.
    TG-1332
    
    Change-Id: I459ae1ebcf8b4aeac0504c5416588883b2bd4df1
    Signed-off-by: fonsecamau <fonsecamau@hpe.com>

--- tests/vlan/test_ft_vlan_removed_from_end_of_table.py
+++ tests/vlan/test_ft_vlan_removed_from_end_of_table.py
@@ -133,6 +133,8 @@ def cleanUp(dut, wrk1, wrk2, wrk3):
     else:
         LogOutput('info', "Passed Switch Reboot piece")
 
+@pytest.mark.skipif(True, reason="Disabling because modular framework tests "
+                                         "were enable")
 class Test_vlan_state_removed_from_end_of_table:
 
     def setup_class(cls):
