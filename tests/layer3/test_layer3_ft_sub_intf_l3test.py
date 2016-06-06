60dd57bda49b932937086a54164597f3df6762f0
fonsecamau <fonsecamau@hpe.com>
2016-06-06 14:47:29 -0700

    chg: dev: Disabling tests. Tests were ported to modular framework.
    Change-Id: I559898bb3bf7c439f6ed767b5d22534443b44d58
    Signed-off-by: fonsecamau <fonsecamau@hpe.com>

--- tests/layer3/test_layer3_ft_sub_intf_l3test.py
+++ tests/layer3/test_layer3_ft_sub_intf_l3test.py
@@ -216,6 +216,8 @@ def deviceCleanup(**kwargs):
         assert(False)
 
 
+@pytest.mark.skipif(True, reason="Disabling because modular framework tests "
+"were enable")
 class Test_subInt:
     def setup_class(cls):
         # Test object will parse command line and formulate the env
